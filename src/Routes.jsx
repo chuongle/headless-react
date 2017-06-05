83df9600a85146a29b6cb9a030461bd7960549bb
Chuong Le <chuong@poeticsystems.com>
2017-06-05 13:08:53 -0500

    build Routes based on alias provided by Drupal

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,18 +3,56 @@ import MainLayout from './components/MainLayout';
 import CommunityFull from './components/community/CommunityFull';
 import Communities from './components/community/Communities';
 import { BrowserRouter as Router, Route, browserHistory } from 'react-router-dom';
+import { gql, graphql } from 'react-apollo';
 
 class Routes extends Component {
+  // render() {
+  //   return (
+  //     <Router history={browserHistory}>
+  //       <MainLayout>
+  //         <Route path="/communities" component={Communities} />
+  //         <Route path="/entity/:nid" component={CommunityFull} />
+  //       </MainLayout>
+  //     </Router>
+  //   );
+  // }
+  renderRoutes() {
+    return this.props.data.nodeQuery.entities.map((entity) => {
+      return <Route key={entity.entityId} path={entity.entityUrl.alias} component={CommunityFull} />
+    });
+  }
+
   render() {
+    if (this.props.data.loading) {
+      return (<div>Loading</div>)
+    }
+
+    if (this.props.data.error) {
+      return (<div>An unexpected error occurred</div>)
+    }
+    
     return (
       <Router history={browserHistory}>
         <MainLayout>
           <Route path="/communities" component={Communities} />
-          <Route path="/entity/:nid" component={CommunityFull} />
+          {this.renderRoutes()}
         </MainLayout>
       </Router>
-    );
+    )
   }
 }
 
-export default Routes;
+// export default Routes;
+export default graphql(gql`query {
+  nodeQuery(filter: {type: "community"}) {
+    entities {
+      ...on NodeCommunity {
+        entityId
+        entityUrl {
+          path
+          alias
+        }
+      }
+    }
+  }
+}`)(Routes)
--- src/client.jsx
+++ src/client.jsx
@@ -2,7 +2,7 @@ import { ApolloClient, createNetworkInterface, gql, graphql } from 'react-apollo
 
 const client = new ApolloClient({
   networkInterface: createNetworkInterface({
-    uri: process.env.DRUPAL_URL,
+    uri: process.env.DRUPAL_URL + process.env.GRAPHQL_URL,
   }),
 });
 
